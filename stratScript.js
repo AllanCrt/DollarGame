f5136574cfec0f51e776ea4bcafafa80b8946d6d
CorentinTeyssier <80049430+CorentinTeyssier@users.noreply.github.com>
2021-04-02 17:15:36 +0200

    Update stratScript.js

--- stratScript.js
+++ stratScript.js
@@ -6,64 +6,111 @@
  }
 
 
-function creation_graphe(taille) {
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
-	//Le graphe complet avec tout les sommets et toutes les arretes
+function generation_graphe(taille, degre){
+	let graphe= new Array();
 
-	let graphe_complet = 
-	[[0,-1,1,3,4],[1,-1,0,2,3,4,5],[2,-1,1,4,5],
-	[3,-1,0,1,4,6,7],[4,-1,0,1,2,3,5,6,7,8],[5,-1,1,2,4,7,8],
-	[6,-1,3,4,7],[7,-1,3,4,5,6,8],[8,-1,4,5,7]];
-	
-	for (let i = 0; i < (9 - taille); i++) {
-		//Recherche d'un sommet aléatoire à supprimer
+	//initialise le graphe
+	for (let i=0;i<taille;i++){
+		graphe[i] = [i,0];
+	}
 
-		sommet_elimine = 4;
-		while(sommet_elimine == 4){
-			position_sommet_elimine = getRandomIntInclusive(0,graphe_complet.length-1);
-			sommet_elimine = graphe_complet[position_sommet_elimine][0];
-		}
-
-		//Suppression des arrètes en lien avec le sommet éliminé
-
-
-		for (let i = 0; i < graphe_complet.length; i++) {
-			const index = graphe_complet[i].indexOf(sommet_elimine);
-			if (index > -1) {
-				graphe_complet[i].splice(index, 1);
+	//ajoute aleatoirement les arretes
+	for(let i = 0;i<taille;i++){
+		for(let j=i+1; j<taille;j++){
+			let rand = Math.random();
+			if(degre > rand){
+				graphe[i].push(j);
+				graphe[j].push(i);
 			}
 		}
-
-		//Suppression du sommet
-
-		graphe_complet.splice(position_sommet_elimine,1);
-
 	}
 
-	//Set up du poids des sommets pour que la somme fasse 0
-	nombre_voisins = 0;
-	for (let i = 0; i < graphe_complet.length; i++) {
-		nombre_voisins += (graphe_complet[i].length - 2);
-	}
-	nombre_arretes = nombre_voisins / 2;
-	poids_total = 0;
-	for (let i = 0; i < graphe_complet.length; i++) {
-		poids = getRandomIntInclusive(-graphe_complet.length,graphe_complet.length);
-		if (i == (graphe_complet.length - 1)) {
-			poids = (nombre_arretes - graphe_complet.length + 1) - poids_total;
+	//retire les sommets qui ne contiennent pas d'arretes
+    let c=0;  
+	for (let i=0;i<taille;i++){
+		if(graphe[i-c].length == 2){
+			graphe.splice(i-c, 1);
+			c++;
 		}
-		poids_total = poids_total + poids;
-		graphe_complet[i][1] = poids;
+	}
+
+	//verifie si le graphe est bien connexe, sinon rappelle la fonction pour en générer un nouveau
+	if(!est_connexe(graphe)){
+		return generation_graphe(taille,degre);
 	}
 
 
-	//Affichage graphe généré
+	//Rempli aléatoirement les poids du graphe
+	let nbr_betti = nombre_betti(graphe);
+	let current_val = 0;
+	for(let i=0;i<graphe.length-1;i++){
+		let sign = Math.random();
+		let value = getRandomInt(graphe.length);
+		if(current_val < nbr_betti){
+			current_val += value;
+			graphe[i][1] = value;
+		}else if(current_val > nbr_betti){
+			current_val -= value;
+			graphe[i][1] = -value;
+		}else{
+			if(sign > 0.5){
+				current_val += value;
+				graphe[i][1] = value;
+			}else{
+				current_val -= value;
+				graphe[i][1] = -value;
+			}
+		}
+	}
+	graphe[graphe.length-1][1] = nbr_betti - current_val;
 
-	// for (let i = 0; i < graphe_complet.length; i++) {
-	// 	console.log(graphe_complet[i]);
-	// }
+	return graphe;
+}
 
-	return graphe_complet;
+function nombre_arrete(graphe){
+	let c=0;
+	for(let i=0; i<graphe.length;i++){
+		c += (graphe[i].length-2);
+	}
+	return c/2;
+}
+
+function nombre_betti(graphe){
+	return nombre_arrete(graphe)-graphe.length+1;
+}
+
+//Verification de la connexite avec l'algo 'composante'
+function est_connexe(graphe){
+	let comp = new Array();
+	for(let i=0; i<graphe.length;i++){
+		comp[graphe[i][0]] = graphe[i][0];
+	}
+	// console.log(JSON.stringify(comp));
+
+	for(let i = 0;i<graphe.length;i++){
+		for (let j = 2;j<graphe[i].length;j++){
+			if(comp[graphe[i][0]] != comp[graphe[i][j]]){
+				aux = comp[graphe[i][0]];
+				for(let k=0;k<graphe.length;k++){
+					if(comp[graphe[k][0]]==aux){
+						comp[graphe[k][0]]=comp[graphe[i][j] ];
+					}
+				}
+			}
+		}
+	}
+	let val = comp[graphe[0][0]];
+	for (let i = 1; i<graphe.length;i++){
+		if(comp[graphe[i][0]] != val){
+			return false;
+		}
+	}
+	return true;
+	
 }
 
 function gagner(){
@@ -77,7 +124,7 @@ function gagner(){
 }
 
 //Trouve le numero du sommet avec la plus grande valeur, si il y en a plusieurs, prend le premier par ordre des numeros de sommet
-function max(){
+function strat1(){
 	var max = -10000000;
 	var maxsommet;
 	for (let i = 0; i < grapheCourant.length; i++){
@@ -88,6 +135,42 @@ function max(){
 	}
 	return maxsommet;
 }
+
+
+function strat2(){
+	var max = -100000;
+	var maxsommet;
+	var sommevoisinsmax = 0;
+	for(let i = 0; i < grapheCourant.length; i++){
+		// console.log('i = '+i);
+		if(grapheCourant[i][1] == max){
+			var sommevoisins = 0;
+			for(let j = 2; j < grapheCourant[i].length; j++){
+				// console.log('sommevoisins += '+grapheCourant[position(grapheCourant[i][j])][1]);
+				sommevoisins += grapheCourant[position(grapheCourant[i][j])][1];
+			}
+			
+			// console.log('somevoisins = '+sommevoisins);
+			if(sommevoisins < sommevoisinsmax){
+				max = grapheCourant[i][1];
+				maxsommet = grapheCourant[i][0];
+			}
+		}else if(grapheCourant[i][1] > max){
+			max = grapheCourant[i][1];
+			maxsommet = grapheCourant[i][0];
+			for(let j = 2; j < grapheCourant[i].length; j++){
+				// console.log('j = '+j);
+				// console.log('sommevoisinsmax += '+grapheCourant[position(grapheCourant[i][j])][1]);
+				sommevoisinsmax += grapheCourant[position(grapheCourant[i][j])][1];
+			}
+			// console.log('somevoisinsmax = '+sommevoisinsmax);
+
+		}
+	}
+	// console.log('maxsommet = '+maxsommet);
+	return maxsommet;
+}
+
 
 function decremente(numsommet){
 	grapheCourant[position(numsommet)][1] =  grapheCourant[position(numsommet)][1] - grapheCourant[position(numsommet)].length + 2;
@@ -115,9 +198,11 @@ function click(numsommet){
 //Applique la stratégie sur 100 coups sauf si le jeu est gagné au milieu des tentatives
 function statStratPlusGrand(){
 	var i = 0;
-	grapheCourant = creation_graphe(6);
-	while(!gagner() && i<100){
-		click(max());
+	grapheCourant = generation_graphe(15,0.25);
+	// console.log(JSON.stringify(grapheCourant));
+	// strat2();
+	while(!gagner() && i<1000){
+		click(strat2());
 		i++;
 	}
 	if(gagner()){
@@ -141,7 +226,7 @@ function test100(){
 //Fonction main pas besoin d'expliqué t'es pas con
 function main(){
 	var test = document.getElementById('test');
-	test.onclick = function(){test100()};
+	test.onclick = function(){test100();};
 }
 
 main();
